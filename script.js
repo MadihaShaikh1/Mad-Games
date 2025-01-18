99df2904fa1f63c1f07d051286125cf5518ba8ab
MadihaShaikh1 <fozenfreefire5@gmail.com>
2025-01-18 17:57:01 +0000

    git commit -m Added 'Start Playing' button functionality to the homepage.

---
+++ script.js
@@ -0,0 +1,13 @@
+function showGame() {
+        alert("Coming soon :< ");
+        }
+
+        function playGame(gameName) {
+            alert("Loading " + gameName + "... Stay tuned!");
+            }
+
+            // Contact form submission
+            document.getElementById("contactForm").addEventListener("submit", function(event) {
+                event.preventDefault();
+                    alert("Thanks for reaching out! We'll get back to you soon.");
+                    });
